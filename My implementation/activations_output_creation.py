911035310d1aef1c3cb65246aeebe7985802aedf
Unknown <alonhare@gmail.com>
2018-10-28 11:18:31 +0200

    saving activations output works

--- My implementation/activations_output_creation.py
+++ My implementation/activations_output_creation.py
@@ -16,6 +16,7 @@ from keras.models import model_from_yaml
 from art.attacks.fast_gradient import FastGradientMethod
 from art.classifiers import KerasClassifier
 from art.utils import load_dataset
+from keras.models import Model
 
 # Read MNIST dataset
 (x_train, y_train), (x_test, y_test), min_, max_ = load_dataset(str('mnist'))
@@ -32,7 +33,30 @@ loaded_model.load_weights(local_path + "model.h5")
 loaded_model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
 
 # sanity check
-classifier = KerasClassifier((min_, max_), model=loaded_model)
-preds = np.argmax(classifier.predict(x_test), axis=1)
-acc = np.sum(preds == np.argmax(y_test, axis=1)) / y_test.shape[0]
-print("\nTest accuracy: %.2f%%" % (acc * 100))
+# classifier = KerasClassifier((min_, max_), model=loaded_model)
+# preds = np.argmax(classifier.predict(x_test), axis=1)
+# acc = np.sum(preds == np.argmax(y_test, axis=1)) / y_test.shape[0]
+# print("\nTest accuracy: %.2f%%" % (acc * 100))
+
+input_shape = x_train[0].shape
+
+model = loaded_model
+
+def getModelOutput(model, layer_name, data):
+    intermediate_layer_model = Model(inputs=model.input,
+                                     outputs=model.get_layer(layer_name).output)
+    intermediate_output = intermediate_layer_model.predict(data)
+    return intermediate_output
+
+
+conv1_output = getModelOutput(model, "conv1", x_test)
+conv2_output = getModelOutput(model,"conv2", x_test)
+
+print("conv1: ")
+print(conv1_output)
+print("conv2: ")
+print(conv2_output)
+print("conv1 output shape: ")
+print(conv1_output.shape)
+print("conv2 output shape: ")
+print(conv2_output.shape)
